72c682b019829145a2ad065b8a001c12f32f1e76
Jia Fu Low <jia.fu.low@cern.ch>
2016-09-18 13:10:26 -0400

    Add L1Trigger/L1TMuonEndCap/test/PerlFiles
    The perl scripts take the ~800 LUT files generated by emtf_geometry-gen.cc
    and make a header of global variables like PhThLUT_Neighbor.h

---
+++ L1Trigger/L1TMuonEndCap/test/PerlFiles/makEHeader.pl
@@ -0,0 +1,84 @@
+#!/usr/bin/perl
+
+use strict; 
+ 
+my ($i, $j, $fileName,@inArray,$arrayName, $station, $chamber); 
+my ($station, $chamber, $stub, $endcap, $sector, $mainCount); 
+ 
+# print("starting script .. \n"); 
+ 
+    $stub    = "th_lut"; 
+ 
+#    $station = 2; 
+#    $chamber = 1; 
+ 
+$j = 0; 
+ 
+for ($station = 2; $station <= 4; $station++){ 
+     
+    for ($chamber = 1; $chamber <= 9; $chamber++){ 
+	 
+	for ($mainCount = 0; $mainCount < 12; $mainCount++){ 
+ 
+	    $j++; 
+ 
+	    @inArray = (); 
+  
+	    {  
+		use integer;  
+		$endcap  = $mainCount / 6 + 1; 
+		$sector  = $mainCount % 6 + 1 ; 
+	    } 
+	     
+	    $arrayName = join "_", $stub, "st", "$station", "ch" , "$chamber"; 
+	    $arrayName = "_$arrayName"; 
+	     
+	    $fileName = join "_", "vl", $stub, "endcap", $endcap, "sec", $sector,  
+	    "st", $station, "ch" , $chamber; 
+	     
+	    $fileName = "$fileName.lut"; 
+	     
+	    open(FILE,"<", $fileName) or die "Can't find file $fileName $!\n"; 
+	     
+	    $i = 0; 
+	     
+	    while (<FILE>){ 
+		chomp; 
+		$inArray[$i] = "0x$_"; 
+		$i++; 
+	    } 
+ 
+	    close (FILE); 
+	     
+	    if ($mainCount == 0){ 
+		print("unsigned int $arrayName\[12\][$i] = {\n"); 
+	    } 
+	     
+	    print ("\n// Endcap: $endcap Sector: $sector"); 
+	     
+	    for ($i = 0; $i < $#inArray; $i++){ 
+		if ($i%10 == 0){ 
+		    print("\n");  
+		    if ($i == 0) { 
+			print("{ "); 
+		    } else { 
+			print("  "); 
+		    } 
+		} 
+		 
+		printf("%5s, ", $inArray[$i]); 
+	    } 
+	     
+	    printf("%5s  %5s",$inArray[$#inArray],"}"); 
+	     
+	    print(",\n\n"); 
+	     
+	} 
+	 
+	print("};\n"); 
+    } 
+ 
+} 
+ 
+print("\n// makeHeader.pl merged $j files\n"); 
+ 
---
+++ L1Trigger/L1TMuonEndCap/test/PerlFiles/st1thlutfull.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/perl
+
+use strict;
+
+my ($i, $j, $fileName,@inArray,$arrayName, $station, $chamber); 
+my ($station, $chamber, $stub, $endcap, $sector, $mainCount); 
+
+# print("starting script .. \n"); 
+ 
+    $stub    = "th_lut"; 
+ 
+#    $station = 2; 
+#    $chamber = 1; 
+ 
+$j = 0; 
+ 
+for ($station = 0; $station <= 1; $station++){ 
+     
+    for ($chamber = 1; $chamber <= 16; $chamber++){ 
+	 
+	for ($mainCount = 0; $mainCount < 12; $mainCount++){ 
+ 
+	    $j++; 
+ 
+	    @inArray = (); 
+  
+	    {  
+		use integer;  
+		$endcap  = $mainCount / 6 + 1; 
+		$sector  = $mainCount % 6 + 1 ; 
+	    } 
+	     
+	    $arrayName = join "_", $stub, "st_1","sub", "$station", "ch" , "$chamber"; 
+	    $arrayName = "_$arrayName"; 
+	     
+	    $fileName = join "_", "vl", $stub, "endcap", $endcap, "sec", $sector,  
+	    "sub", $station+1, "st_1", "ch" , $chamber; 
+	     
+	    $fileName = "$fileName.lut"; 
+	     
+	    open(FILE,"<", $fileName) or die "Can't find file $fileName $!\n"; 
+	     
+	    $i = 0; 
+	     
+	    while (<FILE>){ 
+		chomp; 
+		$inArray[$i] = "0x$_"; 
+		$i++; 
+	    } 
+ 
+	    close (FILE); 
+	     
+	    if ($mainCount == 0){ 
+		print("unsigned int $arrayName\[12\][64] = {\n"); 
+	    } 
+	     
+	    print ("\n// Endcap: $endcap Sector: $sector"); 
+	     
+	    for ($i = 0; $i < 63; $i++){ 
+		if ($i%10 == 0){ 
+		    print("\n");  
+		    if ($i == 0) { 
+			print("{ "); 
+		    } else { 
+			print("  "); 
+		    } 
+		} 
+		 
+		if($i <= $#inArray){
+			printf("%5s, ", $inArray[$i]); 
+		}
+		else{
+			printf("%5s, ", -999);
+		} 
+	    } 
+	     
+	    if($i == $#inArray){
+	   	printf("%5s  %5s",$inArray[$#inArray],"}"); 
+	    }
+	    else{
+	   	printf("%5s  %5s",-999,"}");
+	    }
+	     
+	    print(",\n\n"); 
+	     
+	} 
+	 
+	print("};\n"); 
+    } 
+ 
+} 
+ 
+print("\n// st1thlut.pl merged $j files\n"); 
